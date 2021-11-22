fb942829710a8819ea469c9381753cee562766f7
tsukasaI <inoue.tsukasa.4@gmail.com>
2021-11-22 19:58:02 +0900

    fix headline type

--- react/src/views/pages/Headline.tsx
+++ react/src/views/pages/Headline.tsx
@@ -1,35 +1,42 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 
+
+
+type Coin = {
+  date: String,
+  jpyRate: Number,
+  usdRate: Number,
+}
+
+type Qiita = {
+  date: String,
+  description: String,
+  tags: [{
+    name: String,
+    versions: any
+  }],
+  title: String,
+}
+
+type Weather = {
+  description: String,
+  feel: Number,
+  humidity: Number,
+  pressure: Number,
+  temp: Number,
+  time: String,
+  weather: String,
+}
+
 type HeadlineResponse = {
-  coin: {
-    date: string,
-    jpyRate: string,
-    usdRate: string,
-  },
-  qiita: [{
-    date: string,
-    description: string,
-    title: string,
-    tags: [{
-      name: string,
-      versions: Array<any>,
-    }],
-  }],
-  weather: [{
-    description: string,
-    feel: Number,
-    humidity: Number,
-    pressure: Number,
-    temp: Number,
-    time: string,
-    weather: string,
-  }],
+  coin: Coin,
+  weather: Weather[],
+  qiita: Qiita[],
 }
 
 const Headline = () => {
-  let tmp: HeadlineResponse | undefined
-  const [response, setResponse] = useState(tmp)
+  const [response, setResponse] = useState<HeadlineResponse>()
   useEffect(() => {
     const fetchHeadline = async () => {
       const res = await axios.get('http://localhost:8087/')
